601769e046332bb544520aa4c5caf29136cd2742
[manvanth] <manvanth.me@gmail.com>
2024-09-23 23:53:38 +0530

    middlewares

---
+++ Week-3/Middleware.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/health-checkup",function(req,res){
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = rew.query.kidneyId;
+})
