7085ad982da77c453c176bbe3a483fcc6192ae93
[manvanth] <manvanth.me@gmail.com>
2024-10-03 23:36:59 +0530

    deleted some files

--- Week-3/Middlewares.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/health-checkup",function(req,res){
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = rew.query.kidneyId;
-})
