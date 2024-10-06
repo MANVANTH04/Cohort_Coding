5e3bea4bada1c5fe60dc32768741aa8633275114
[manvanth] <manvanth.me@gmail.com>
2024-10-06 23:56:27 +0530

    completed week-4.1

--- Week-4/index.js
+++ Week-4/index.js
@@ -1,7 +1,15 @@
-var name = "manvanth"
+import chalk from "chalk"
+import {fileURLToPath} from "url"
+import { dirname } from "path"
 
-console.log(name)
 
-var age = "20"
 
-console.log(age)
+console.log(chalk.blue("hello world"))
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  =dirname(__filename)
+
+
+
+// console.log(__dirname)
