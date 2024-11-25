5cdd5d423ed3f6b8e8f30e2e2098b2bf864287dd
[manvanth] <manvanth.me@gmail.com>
2024-11-26 00:16:06 +0530

    commander package

--- Week-4/index.js
+++ Week-4/index.js
@@ -1,15 +1,24 @@
-import chalk from "chalk"
-import {fileURLToPath} from "url"
-import { dirname } from "path"
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
 
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
 
+program.command('count')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split('\n').length;
+        console.log(`There are ${lines} lines in ${file}`);
+      }
+    });
+  });
 
-console.log(chalk.blue("hello world"))
-console.log(chalk.black("hello"))
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname  =dirname(__filename)
-
-
-
-// console.log(__dirname)
+program.parse();
