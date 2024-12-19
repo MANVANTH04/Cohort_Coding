eca04764dc570146f91f09975ca965a6345266f2
[manvanth] <manvanth.me@gmail.com>
2024-12-19 23:56:56 +0530

    mes

--- Week-4/index.js
+++ Week-4/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 
@@ -71,8 +73,13 @@ app.put('/todo/:id', (req, res)=>{
 })
 
 app.delete('/todo/:id', (req,res)=>{
-    
+
 })
+
+
+
+
+
 
 
 app.listen(3000)
